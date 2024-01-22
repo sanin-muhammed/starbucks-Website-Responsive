01aae4675fb6dc9d025f871fcb7ff14cf6f6c5ee
saninmuhammed9633 <146106263+saninmuhammed9633@users.noreply.github.com>
2024-01-22 15:59:55 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,20 @@
+
+const navigation = document.querySelector(".navigation");
+const input = document.querySelector('.input');
+const navItems = document.querySelector('.nav-items');
+const togglerBtn = document.querySelector('.toggler-btn');
+const body = document.body;
+
+
+
+
+
+if (togglerBtn.style.background == "#7c787800" ) {
+    
+    togglerBtn.style.background = "red";
+
+}    
+
+
+
+
